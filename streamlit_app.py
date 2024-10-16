4ba4a8136d158db9164827d7111ee5924c3846aa
xinyi-wang02 <xinyi_wang2@hms.harvard.edu>
2024-10-16 17:52:03 -0400

    debug 2 for pain_chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,7 +84,7 @@ alc_chart = alt.Chart(agg_data1).mark_bar().encode(
 ).interactive()
 
 ## Count of Different Symptoms in Chest Pain ##
-columns_to_analyze2 = ['CDQ001','CDQ006','secondary_symptom']
+columns_to_analyze2 = ['secondary_symptom']
 df1 = df[columns_to_analyze2]
 pain_df = df1[df1['secondary_symptom'].notna()]
 
