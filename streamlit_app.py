8e78a4ee74e0208f8585dda66b300c72052ed96d
xinyi-wang02 <xinyi_wang2@hms.harvard.edu>
2024-10-16 18:10:26 -0400

    alc_chart size adjust back

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,8 +92,8 @@ alc_chart = alt.Chart(agg_data1).mark_arc().encode(
     tooltip=['Frequency', 'Count']
 ).properties(
     title='Frequency of Alcohol intake within a year',
-    width=400,
-    height=200
+    width=600,
+    height=400
 ).interactive()
 
 ## Count of Different Symptoms in Chest Pain ##
