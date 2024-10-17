46e508c1a4cde49e427c56d6d72d1a01b4694e0b
Jiayi_chen_gaia <139523298+GaiaChancjy@users.noreply.github.com>
2024-10-16 22:05:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -129,8 +129,8 @@ heatmap = alt.Chart(subset2).mark_rect().encode(
     tooltip=['alc_Frequency', 'CDQ001', 'Count']
 ).properties(
     title=f'Heat Map of relationship between Alcohol Frequency: {alc_cons} and Chest Pain',
-    width=300,
-    height=600
+    width=100,
+    height=200
 )
 
 ## Count of Different Symptoms in Chest Pain ##
